20fa8fc67628d054f859ea3d42b3e1f6a9ae9d75
jacpuertomadero-beep <jacpuertomadero@gmail.com>
2026-06-09 23:34:42 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -8,10 +8,10 @@ define('APP_NAME', 'JAC Afiliados');
 define('ROOT_PATH', dirname(__DIR__));
 define('APP_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'app');
 
-define('DB_HOST', 'localhost');
-define('DB_PORT', '5432');
-define('DB_NAME', 'jac');
-define('DB_USER', 'postgres');
-define('DB_PASS', 'root');
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_PORT', getenv('DB_PORT') ?: '5432');
+define('DB_NAME', getenv('DB_NAME') ?: 'postgres');
+define('DB_USER', getenv('DB_USER') ?: 'postgres');
+define('DB_PASS', getenv('DB_PASS') ?: 'F@u3b8k3&.AAjdi');
 
 define('BASE_URL', '/');
